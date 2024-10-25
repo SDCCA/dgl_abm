9128c1e332ced3e95f3a936a2d28dc0cc9612bb4
Meiert Grootes <mwgrootes@Meierts-MacBook-Pro.local>
2024-10-25 14:06:32 +0200

    first commit

---
+++ src/dgl_abm/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about dgl_abm."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Meiert W. Grootes"
+__email__ = "m.grootes@esciencecenter.nl"
+__version__ = "0.1.0"
