788d343a72202b05618129da1b30c7848b4d8a95
Victoria <victoria@uvavpn-byodm-145-18-161-71.vpn.uva.nl>
2025-01-07 11:43:54 -0800

    Revert "changed conda source directory due to very annoying Snellius permissions issue after new year."
    This reverts commit 6e7f825912f7db4c1ae491c7c1f06b3d03d94e8a.

--- Experiments/no_social_run.sh
+++ Experiments/no_social_run.sh
@@ -13,7 +13,7 @@ log_file="no_social_run_times.log"
 > "$log_file" 
 
 # Environment (Snellius specific)
-source /home/user/anaconda3/etc/profile.d/conda.sh
+source /sw/arch/Centos8/EB_production/2021/software/Anaconda3/2021.05/etc/profile.d/conda.sh
 #conda env create -f ../environment.yml --name dgl_ptm_gpu
 conda activate dgl_ptm_gpu
 
