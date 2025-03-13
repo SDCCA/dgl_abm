f132d1e466e2e0a58ec5082569866461518cb27b
Victoria <victoria@Victorias-MacBook-Pro.local>
2025-03-13 18:27:04 +0100

    more de-linting

--- dgl_ptm/dgl_ptm/network/local_attachment_tensor.py
+++ dgl_ptm/dgl_ptm/network/local_attachment_tensor.py
@@ -1,48 +1,71 @@
-#noqa
-
+"""Local attachment tensor."""
+import dgl
 import torch
-import dgl 
 from dgl.sparse import spmatrix
 
-import dgl_ptm.util.matrix_utils as matrix_utils
+from dgl_ptm.util import matrix_utils
 
 # TODO: check readability variables.
 
 # TODO: confirm that the behavior for sum(weight)=0 agents is the same as for plain
 #  local attachment
-def local_attachment_tensor(graph,n_FoF_links,edge_prop=None,p_attach=1.):
+def local_attachment_tensor(graph,n_FoF_links,edge_prop=None,p_attach=1.):#noqa N806
+    """Update graph with local attachment based on edge property.
+
+    Args:
+        graph (DGLGraph): All agent node and edge data
+        n_FoF_links (int): Number of new links to attempt
+        edge_prop (str): edge property to use for local attachment
+        p_attach (float): probability of attaching a new edge
+    
+    Returns:
+        None    
+    """
     adj_matrix = adjacency_matrix_with_edge_prop(graph,eprop=edge_prop)
     norm_prop = adj_matrix.val/adj_matrix.val.sum()
 
     #sample n_FoF_links from the entire normalized edge property graph weighted by 
     # edge weight
-    selected_links = norm_prop.multinomial(num_samples=n_FoF_links,replacement=False)
+    selected_links = norm_prop.multinomial(num_samples=n_FoF_links,#noqa N806
+                                           replacement=False)
     selected_links_matrix = matrix_utils.apply_mask_to_sparse_matrix(adj_matrix, 
                                                                      selected_links)
-    FoF_field_matrix, FoF_field_matrix_norm_eprop = neighbour_field_matrix(
+    FoF_field_matrix, FoF_field_matrix_norm_eprop = neighbour_field_matrix(#noqa N806
                                                                 selected_links_matrix,
                                                                 adj_matrix)
-    new_FoF = FoF_field_matrix.val > 0
-    new_FoF_norm_eprop = matrix_utils.apply_mask_to_sparse_matrix(
+    new_FoF = FoF_field_matrix.val > 0#noqa N806
+    new_FoF_norm_eprop = matrix_utils.apply_mask_to_sparse_matrix(#noqa N806
                                                 FoF_field_matrix_norm_eprop, new_FoF)
     if torch.count_nonzero(new_FoF) <= n_FoF_links:
         probe_p_attach = torch.rand(new_FoF_norm_eprop.val.shape[0]) 
         to_link = probe_p_attach < p_attach
     else:
-        new_FoF_renorm = new_FoF_norm_eprop.val/new_FoF_norm_eprop.val.sum()
-        selected_FoF = new_FoF_renorm.flatten().multinomial(num_samples=n_FoF_links,
+        new_FoF_renorm = new_FoF_norm_eprop.val/new_FoF_norm_eprop.val.sum()#noqa N806
+        selected_FoF = new_FoF_renorm.flatten().multinomial(#noqa N806
+                                                            num_samples=n_FoF_links,
                                                             replacement=False)
         #probe_p_attach = torch.rand(new_FoF_norm_eprop.val.shape[0])
         probe_p_attach = torch.rand(n_FoF_links)
         #to_link = torch.logical_and((probe_p_attach > p_attach),selected_FoF)
         probe_selected = probe_p_attach < p_attach
         to_link = selected_FoF[probe_selected]
-    FoF_to_link_matrix = matrix_utils.apply_mask_to_sparse_matrix(new_FoF_norm_eprop, 
+    FoF_to_link_matrix = matrix_utils.apply_mask_to_sparse_matrix(#noqa N806
+                                                                  new_FoF_norm_eprop, 
                                                                   to_link)
     graph.add_edges(FoF_to_link_matrix.row,FoF_to_link_matrix.col)
     graph.add_edges(FoF_to_link_matrix.col,FoF_to_link_matrix.row)
 
 def adjacency_matrix_with_edge_prop(graph,etype=None, eprop=None):
+    """Create adjacency matrix with edge property.
+    
+    Args:
+        graph (DGLGraph): All agent node and edge data
+        etype (str): edge type
+        eprop (str): edge property
+    
+    Returns:
+        spmatrix: adjacency matrix with edge property
+    """
     etype = graph.to_canonical_etype(etype)
     indices = torch.stack(graph.all_edges(etype=etype))
     shape = (graph.num_nodes(etype[0]),graph.number_of_nodes(etype[2]))
@@ -123,6 +146,16 @@ def construct_neighbour_field_tensors(selm,adjm):
     return (rowtensor, coltensor, ltensor, valtensor)
 
 def neighbour_field_matrix(selm, adjm):
+    """Create neighbour field matrix.
+
+    Args:
+        selm (dgl.sparse.spmatrix): matrix of selected edges in sparse format
+        adjm (dgl.sparse.spmatrix): adjacency matrix with edge weights as sparse values
+
+    Returns:
+        nfmc (dgl.sparse.spmatrix): link neighbour field matrix
+        nfmvc (dgl.sparse.spmatrix): value neighbour field matrix
+    """
     nf = construct_neighbour_field_tensors(selm,adjm)
      #stack indices into tensor for matrix construction
     indices= torch.stack((nf[0],nf[1]))
