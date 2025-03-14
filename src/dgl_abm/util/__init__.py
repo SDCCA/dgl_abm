b8a0b4317f6d7359930da5723d34a4b67ae7df2f
Victoria <victoria@wcw-staff-145-109-89-43.wireless.uva.nl>
2025-03-14 13:15:20 +0100

    update to utility

--- src/dgl_abm/util/__init__.py
+++ src/dgl_abm/util/__init__.py
@@ -1,6 +1,7 @@
 """dgl_abm.util - Supply functions for the model and serve as storage for support files.
 
 Submodules:
-- network_utils: Utility functions for network creation
-- utils: Utility functions for model
+- matrix_utils: Utility functions for matrix manipulation
+- utils: Miscellaneous utility functions for model
+- metrics: Metrics for model, agent, and network evaluation
 """
