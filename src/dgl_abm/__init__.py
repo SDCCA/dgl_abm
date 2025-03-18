dda6ec7ca159ffb7e1d192c04286e286a624326a
Meiert Grootes <mwgrootes@Meierts-MacBook-Pro.local>
2025-03-18 13:39:13 +0100

    basic commit to create branch

--- src/dgl_abm/__init__.py
+++ src/dgl_abm/__init__.py
@@ -2,8 +2,10 @@
 
 import logging
 
+__all__ =['config']
+
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__author__ = "Meiert W. Grootes"
+__author__ = "Team Atlas, Meiert W. Grootes"
 __email__ = "m.grootes@esciencecenter.nl"
 __version__ = "0.1.0"
