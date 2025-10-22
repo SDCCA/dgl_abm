66514db1d0f11bba0eab5b481c67777c51684fb4
Victoria <Victoria.Garibay@gmail.com>
2025-10-22 21:30:10 +0200

    changed python range in toml to include 3.10 for macos, added back network and grid creation (no tests)

---
+++ src/dgl_abm/environment/grid_creation.py
@@ -0,0 +1,224 @@
+"""This module pertains to spatial grid creation.
+
+Classes:
+- GridEnvironment: Represents a grid environment with properties for each location
+
+Functions:
+- grid_creation: Creates a representation of the spatial environment
+- grid_creation_3d: Creates a representation of a 3-D spatial environment
+"""
+import numpy as np
+import torch
+from dgl_abm.util.utils import sample_distribution_tensor
+
+# grid_creation - Creates a representation of the spatial environment
+# within which agents act and interact.
+
+class GridEnvironment:
+    """This class represents a spatial grid environment with properties."""
+    def __init__(self, grid_tensor:torch.Tensor, property_index:dict, space:str) -> None:
+        """Initialize the grid environment.
+
+        Args:
+            grid_tensor (torch.Tensor): tensor storing the grid environment properties
+            property_index (dict): dictionary mapping property names to indices
+            space (str): dimensionality of the grid environment (2D or 3D)
+        """
+        self.grid_tensor = grid_tensor
+        self.property_to_index = property_index
+        self.space = space
+        self.grid_shape = grid_tensor.shape
+
+    def get_slice(self, grid_property: str) -> torch.Tensor:
+        """Return the layer of the grid tensor corresponding to a given property.
+
+        Arg:
+            grid_property (str): name of the property for which the slice is requested
+        """
+        if grid_property not in self.property_to_index:
+            property_message = f'Property "{grid_property}" not found.'
+            raise KeyError(property_message)
+        index = self.property_to_index[grid_property]
+        if self.space == "2D":
+            dimensions = 2
+            if len(self.grid_tensor.shape) == dimensions:
+                return self.grid_tensor
+            return self.grid_tensor[:, :, index]
+        if self.space == "3D":
+            dimensions = 3
+            if len(self.grid_tensor.shape) == dimensions:
+                return self.grid_tensor
+            return self.grid_tensor[:, :, :, index]
+        space_message = 'Grid environment space must be either "2D" or "3D".'
+        raise ValueError(space_message)
+
+    def __getitem__(self, grid_property: str) -> torch.Tensor:
+        """Return the layer of the grid tensor corresponding to a given property.
+
+        Arg:
+            grid_property (str): name of the property for which the index is requested
+        """
+        return self.get_slice(grid_property)
+
+def grid_creation(method: str, **kwargs: dict) -> GridEnvironment:
+    """Create a representation of the model environment.
+
+    Args:
+        method (str): Currently implementable methods include:
+            basic:
+                This method creates a uniform grid environment and
+                requires the following keyword arguments,
+                x (int): length of the grid in the x-direction/longitude
+                y (int): length of the grid in the y-direction/latitude
+            distribution:
+                This method creates a grid environment with properties and
+                requires the following keyword arguments,
+                x (int): length of the grid in the x-direction/longitude
+                y (int): length of the grid in the y-direction/latitude
+                properties (dict): dictionary of property distribution dictionaries
+                    to be assigned to the grid (format example,
+                    {"property1": {"type": "uniform", "parameters": [0, 1],
+                            "round": False, "decimals": None}})
+            custom_import:
+                This method creates a grid environment by importing a
+                numpy array or torch tensor from a file (.np or .pt),
+                where each element represents a position with a
+                value coresponding to each property/channel,
+                and requires the following keyword arguments:
+                path (str): path at which .np or .pt file is located
+                properties (dict): dictionary of property names to be assigned
+                    to the third dimension (format example {"property1": 0,
+                    "property2": 1})
+
+        kwargs (dict): keyword arguments to be supplied to the grid creation method
+
+    Return:
+        GridEnvironment: Grid environment created via the specified method
+    """
+    if method == "basic":
+        x = kwargs["x"]
+        y = kwargs["y"]
+        grid = torch.ones(x, y)
+        return GridEnvironment(grid, {"ones": 0},"2D")
+
+    if method == "distribution":
+        x = kwargs["x"]
+        y = kwargs["y"]
+        properties = kwargs["properties"]
+        grid = torch.zeros(x, y, len(properties))
+        n = x * y
+        for i, prop in enumerate(properties):
+            distribution = properties[prop]
+
+            grid[:, :, i] = sample_distribution_tensor(distribution["type"],
+                            distribution["parameters"], n,
+                            round = distribution["round"],
+                            decimals = distribution["decimals"]).reshape(x, y)
+        return GridEnvironment(grid, {key: i for i, key in enumerate(
+                                                                    properties.keys())},
+                                                                                "2D")
+
+    if method == "custom_import":
+        if "path" not in kwargs:
+            value_message = ("Path to grid tensor must be provided for"
+                              '"custom_import" method.')
+            raise ValueError(value_message)
+        path = kwargs["path"]
+        properties = kwargs["properties"]
+        if path.endswith((".np",".npy")):
+            grid = torch.from_numpy(np.load(path))
+        elif path.endswith(".pt"):
+            grid = torch.load(path)
+        else:
+            value_message = ("File type not supported for grid creation; please use "
+                                '".npy", ".pt", or ".np".')
+            raise ValueError(value_message)
+        return GridEnvironment(grid, properties,"2D")
+    message = "Unsupported grid creation method received."
+    raise NotImplementedError(message)
+
+
+
+def grid_creation_3d(method: str, **kwargs: dict) -> GridEnvironment:
+    """Create a representation of the model environment in three dimensions.
+
+    Args:
+        method (str): Currently implementable methods include:
+            basic:
+                This method creates a uniform grid environment and
+                requires the following keyword arguments,
+                x (int): length of the grid in the x-direction/longitude
+                y (int): length of the grid in the y-direction/latitude
+                z (int): length of the grid in the z-direction/depth
+            distribution:
+                This method creates a grid environment with properties and
+                requires the following keyword arguments,
+                x (int): length of the grid in the x-direction/longitude
+                y (int): length of the grid in the y-direction/latitude
+                z (int): length of the grid in the z-direction/depth
+                properties (dict): dictionary of property distribution dictionaries
+                    to be assigned to the grid (format example,
+                    {"property1": {"type": "uniform", "parameters": [0, 1],
+                            "round": False, "decimals": None}})
+            custom_import:
+                This method creates a grid environment by importing a
+                numpy array or torch tensor from a file (.np or .pt),
+                where each element represents a position in the lattice with a
+                value coresponding to each property/channel,
+                and requires the following keyword arguments:
+                path (str): path at which .np or .pt file is located
+                properties (dict): dictionary of property names to be assigned
+                    to the third dimension (format example {"property1": 0,
+                    "property2": 1})
+
+        kwargs (dict): keyword arguments to be supplied to the grid creation method
+
+    Return:
+        GridEnvironment: Grid environment created via the specified method
+    """
+    if method == "basic":
+        x = kwargs["x"]
+        y = kwargs["y"]
+        z = kwargs["z"]
+        grid = torch.ones(x, y, z)
+        return GridEnvironment(grid, {"ones": 0},"3D")
+
+    if method == "distribution":
+        x = kwargs["x"]
+        y = kwargs["y"]
+        z = kwargs["z"]
+        properties = kwargs["properties"]
+        grid = torch.zeros(x, y, z, len(properties))
+        n = x * y * z
+        for i, prop in enumerate(properties):
+            distribution = properties[prop]
+
+            grid[:, :, :, i] = sample_distribution_tensor(distribution["type"],
+                            distribution["parameters"], n,
+                            round = distribution["round"],
+                            decimals = distribution["decimals"]).reshape(x, y, z)
+        return GridEnvironment(grid, {key: i for i, key in enumerate(
+                                                                    properties.keys())},
+                                                                                "3D")
+
+    if method == "custom_import":
+        if "path" not in kwargs:
+            value_message = ("Path to grid tensor must be provided for"
+                              '"custom_import" method.')
+            raise ValueError(value_message)
+        path = kwargs["path"]
+        properties = kwargs["properties"]
+        if path.endswith((".np",".npy")):
+            grid = torch.from_numpy(np.load(path))
+        elif path.endswith(".pt"):
+            grid = torch.load(path)
+        else:
+            value_message = ("File type not supported for grid creation; please use "
+                                '".npy", ".pt", or ".np".')
+            raise ValueError(value_message)
+        return GridEnvironment(grid, properties,"3D")
+    message = "Unsupported grid creation method received."
+    raise NotImplementedError(message)
+
+
+
---
+++ src/dgl_abm/network/network_creation.py
@@ -0,0 +1,78 @@
+"""This module contains functions to initialize a network for agents in the model.
+
+Function(s):
+- network_creation: Generates the network between the initialized nodes
+- barabasi_albert_graph: Generates a barabasi-albert graph
+"""
+
+import logging
+import dgl
+import networkx as nx
+import torch
+
+logger = logging.getLogger(__name__)
+
+def network_creation(num_agents: int, method: str, **kwargs: dict) -> dgl.DGLGraph:
+    """Create the network between the initialized nodes using edges from DGL.
+
+    Args:
+        num_agents (int): Number of agent nodes
+        method (str): Network creation method
+            barabasi_albert:
+                This method takes the following possible keyword arguments,
+                seed: random seed for networkx barabasi_albert_graph function
+                new_node_edges: number of edges to create for each new node
+        kwargs (dict): keyword arguments to be supplied to the network creation method
+
+    Return:
+        agent_graph (DGLGraph): network of agent nodes resulting from the chosen method
+    """
+    if (method == "barabasi-albert"):
+        seed  = kwargs["seed"] if "seed" in kwargs else torch.initial_seed()
+
+        new_node_edges = kwargs.get("new_node_edges", 1)
+        info_message = (f"Using seed {seed} for network creation with {new_node_edges} "
+                        "edges requested.")
+        logger.info(info_message)
+        agent_graph = barabasi_albert_graph(num_agents, new_node_edges, seed)
+    elif method in ["empty", "edgeless", "null", "disconnected", "none", None]:
+        agent_graph = edgeless_graph(num_agents)
+    else:
+        message = "Currently only barabasi-albert network creation is supported."
+        raise NotImplementedError(message)
+
+    return agent_graph
+
+def barabasi_albert_graph(num_agents: int, new_node_edges: int = 1,
+                          seed: int = 1) -> dgl.DGLGraph:
+    """Create a barabasi-albert graph.
+
+    This function creates a network graph for user-defined
+    number of agents using the barabasi albert model function
+    from networkx.
+
+    Args:
+        num_agents (int): number of agent nodes
+        new_node_edges (int): number of edges to create for each new node
+        seed (int): random seed for function
+
+    Return:
+        agent_graph (DGLGraph): network of agent nodes resulting from the chosen method
+    """
+    #Create graph using networkx function for barabasi albert graph
+    networkx_graph = nx.barabasi_albert_graph(n=num_agents, m=new_node_edges, seed=seed)
+    barabasi_albert_coo = nx.to_scipy_sparse_array(networkx_graph,format="coo")
+
+    #Return DGL graph from networkx graph
+    return dgl.from_scipy(barabasi_albert_coo)
+
+def edgeless_graph(num_agents: int) -> dgl.DGLGraph:
+    """Create an empty graph with the given number of nodes.
+
+    Args:
+        num_agents (int): Number of agent nodes
+
+    Returns:
+        agent_graph (DGLGraph): DGL graph with nodes and no edges
+    """
+    return dgl.graph(([], []), num_nodes=num_agents)
