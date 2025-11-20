384c446034ad81a454a0d7c2ce9cdf8da241dcb6
Victoria <Victoria.Garibay@gmail.com>
2025-11-20 04:23:43 +0100

    lint and format fixes

--- demo/run_cpu_default.py
+++ demo/run_cpu_default.py
@@ -1,11 +1,12 @@
+"""Demo script to run model on CPU with default configuration."""
+
 import os
+
 os.environ["DGLBACKEND"] = "pytorch"
 
 import dgl_abm
 
-
-model = dgl_abm.Model(experiment_identifier=f'default_demo')
+model = dgl_abm.Model(experiment_identifier="default_demo")
 model.set_model_parameters()
 model.initialize_model()
 model.run()
-
